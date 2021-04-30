99bd23a4a4f793f940eb0282efbf3260ec3bdd44
unknown <f20170855@hyderabad.bits-pilani.ac.in>
2021-05-01 03:39:52 +0530

    fully functional youtube clone deployed using firebase

---
+++ build/precache-manifest.7806e4a33d62db4fb239dea4654da252.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bee73270a83c2562d0f17a7c8495529",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81bc4c9163cae0b8b3dd",
+    "url": "/static/css/main.ff03f03e.chunk.css"
+  },
+  {
+    "revision": "b4d519b9880aa253b5d7",
+    "url": "/static/js/2.a27e2320.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.a27e2320.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81bc4c9163cae0b8b3dd",
+    "url": "/static/js/main.f60ea3bd.chunk.js"
+  },
+  {
+    "revision": "430e6bf337df4dfe427b",
+    "url": "/static/js/runtime-main.8dbc5147.js"
+  }
+]);
